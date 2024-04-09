23e96713e8e4d1c2ae943e71f58e43b702cd29e1
Joseph DeWeese <inclinedtactics@icloud.com>
2024-04-09 14:14:55 -0500

    Initial Commit

---
+++ DailyWorkout/DailyWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyWorkoutApp.swift
+//  DailyWorkout
+//
+//  Created by Joseph DeWeese on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
